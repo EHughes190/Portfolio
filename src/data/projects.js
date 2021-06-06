e08de2b76993a125590ca10b279a5f7f633f1754
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-06-06 13:15:26 +0100

    Created projects data file

---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    title: "Flappy Clone",
+    img: "https://drive.google.com/file/d/1iw7YuEFkScd7cfvoVSPDedzmIMAwIJQG/view?usp=sharing",
+    githubUrl: "https://github.com/EHughes190/Flappy-Clone",
+    pagesUrl: "https://ehughes190.github.io/Flappy-Clone/",
+    skills: ["HTML", "SCSS", "JavaScript"],
+    description:
+      "A browser based, infinite runner game, built using HTML, SCSS and JavaScript. The core functionality of the game is created using JavaScript. The game features a session based highscore system, and crisp collision detection. Although the game is best enjoyed on a mobile/ portrait device, it is scalable to any device width.",
+  },
+
+  {
+    title: "JS Calculator",
+    img: "https://drive.google.com/file/d/1y0sa9038mNSdFNle8u0ARRdVMbgU94_-/view?usp=sharing",
+    githubUrl: "https://github.com/EHughes190/JS-Calculator",
+    pagesUrl: "https://ehughes190.github.io/JS-Calculator/",
+    skills: ["HTML", "SCSS", "JavaScript", "Cypress"],
+    description:
+      "A calculator built with JavaScript, HTML and SCSS. This project allowed me to practise my JS skills and fundamentals, as well as implement e2e testing using cypress.",
+  },
+
+  {
+    title: "Morse Translator",
+    img: "https://drive.google.com/file/d/1GNh0jJET9faNuolS1UFY7gHOR_HLHaaw/view?usp=sharing",
+    githubUrl: "https://github.com/EHughes190/morse-translator",
+    pagesUrl: "https://ehughes190.github.io/morse-translator/",
+    skills: ["HTML", "SCSS", "JavaScript", "Jest"],
+    description:
+      "A transaltor app built using HTML, SCSS AND JavaScript. This project allows an input to be translated from English to Morse, and vice versa. I was able to implement Unit testing using Jest in this project. The main functionality of the translation is based around two functions, which contain an object of alpha/numeric key: value pairs.",
+  },
+];
