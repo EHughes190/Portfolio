ba7030f46eb9cecac8ad99dbc5bab96000f80901
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-06-08 19:50:06 +0100

    animation for nav modal, click events for  links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,37 +3,39 @@ import styles from "./Navbar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faIgloo } from "@fortawesome/free-solid-svg-icons";
 import { NavLink, Link } from "react-router-dom";
-import { Squash as Hamburger } from "hamburger-react";
+import { Twirl as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
   const [isOpen, setisOpen] = useState(false);
-
-  const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" },
+  const setActive = () => {
+    setisOpen(!isOpen);
   };
 
   const openNav = isOpen ? (
-    <div className={styles.menu}>
-      <ul className={styles.links}>
-        <NavLink exact to="projects" key="nav:projects">
-          <li className={styles.link__item}>Projects</li>
-        </NavLink>
-        <NavLink exact to="about" key="nav:about">
-          <li className={styles.link__item}>About</li>
-        </NavLink>
-        <NavLink exact to="contact" key="nav:contact">
-          <li className={styles.link__item}>Contact</li>
-        </NavLink>
-      </ul>
+    <div className={styles.modal}>
+      <div className={styles.menu}>
+        <ul className={styles.links}>
+          <NavLink exact to="projects" key="nav:projects">
+            <li className={styles.link__item} onClick={setActive}>
+              Projects
+            </li>
+          </NavLink>
+          <NavLink exact to="about" key="nav:about">
+            <li className={styles.link__item} onClick={setActive}>
+              About
+            </li>
+          </NavLink>
+          <NavLink exact to="contact" key="nav:contact">
+            <li className={styles.link__item} onClick={setActive}>
+              Contact
+            </li>
+          </NavLink>
+        </ul>
+      </div>
     </div>
   ) : (
     ""
   );
-
-  const setActive = () => {
-    setisOpen(!isOpen);
-  };
 
   return (
     // <div className={styles.sticky}>
@@ -42,7 +44,7 @@ const Navbar = () => {
         <FontAwesomeIcon icon={faIgloo} size="2x" />
       </NavLink>
       {openNav}
-      <Hamburger onToggle={setActive} />
+      <Hamburger toggled={isOpen} onToggle={setActive} />
     </nav>
     // </div>
   );
