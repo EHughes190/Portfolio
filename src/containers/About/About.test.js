155bb652ea7e6961d06178af244c457a0ffba76c
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2022-07-02 13:24:39 +0100

    chore: file clean up

--- src/containers/About/About.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import About from "./About";
-
-describe("About tests", () => {
-  it("should render", () => {
-    expect(render(<About />)).toBeTruthy();
-  });
-});
--- src/containers/Contact/Contact.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Contact from "./Contact";
-
-describe("Contact tests", () => {
-  it("should render", () => {
-    expect(render(<Contact />)).toBeTruthy();
-  });
-});
